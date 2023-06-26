54a1bdf7df475ecc499e54acb006d2d784cf2adc
ViniciusMufalo <133791110+ViniciusMufalo@users.noreply.github.com>
2023-06-26 10:18:04 -0300

    Primeira adição

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+
+//efeito de aparecer aos poucos:
+window.sr = ScrollReveal({reset:true});
+
+sr.reveal('.inicio_descricao_e_img, .inicio_link', {duration: 1000});
+
+window.sr = ScrollReveal({reset:true});
+
+sr.reveal('.sobre_mim_conteudo, .projeto_link, .contato_link', {duration: 2000});
+
+
+
+
+
