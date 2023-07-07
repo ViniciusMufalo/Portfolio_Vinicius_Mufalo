8c87883d212241bb07151189292583cb6cb9131f
ViniciusMufalo <133791110+ViniciusMufalo@users.noreply.github.com>
2023-07-07 13:40:47 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,3 @@
-
 //efeito de aparecer aos poucos:
 window.sr = ScrollReveal({reset:true});
 
@@ -6,9 +5,43 @@ sr.reveal('.inicio_descricao_e_img, .inicio_link', {duration: 1000});
 
 window.sr = ScrollReveal({reset:true});
 
-sr.reveal('.sobre_mim_conteudo, .projeto_link, .contato_link', {duration: 2000});
+sr.reveal('.sobre_mim_conteudo', {duration: 2000});
+//efeito de aparecer aos poucos:
 
 
 
+//efeito abas dos icones em habilidades
+function abrirTabIconesHabilidade(idTab) {
+    var habilidades = document.getElementsByClassName('icone_conteudo');
+
+    for (var i = 0; i < habilidades.length; i++) {
+        habilidades[i].style.display = 'none';
+    }
+
+    document.getElementById(idTab).style.display = 'block';
+}//efeito abas dos icones em habilidades
 
 
+//Ver Mais/Ver Menos
+
+var mostrarProjetos = document.getElementsByClassName('maisProjetos')[0]
+var verMenosBotao = document.getElementsByClassName('verMenosBotao')[0]
+var verMaisBotao = document.getElementsByClassName('verMaisBotao')[0]
+
+function mostrar(){
+    
+    mostrarProjetos.style.display = 'block'
+    verMenosBotao.style.display = 'block'
+    verMenosBotao.style.marginLeft = "46%"
+    verMaisBotao.style.display = 'none'
+}
+
+function esconder(){
+    
+    mostrarProjetos.style.display = 'none'
+    verMenosBotao.style.display = 'none'
+    verMaisBotao.style.display = 'block'
+    verMaisBotao.style.marginLeft = "46%"
+}
+
+//Ver Mais/Ver Menos
